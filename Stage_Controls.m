3b9619f821c82bf77b62527fe604de5231ec0f54
Mathmatician <38021845+Mathmatician@users.noreply.github.com>
2020-04-17 10:51:37 -0600

    Create Stage_Controls.m

---
+++ Stage_Controls.m
@@ -0,0 +1,73 @@
+clear all; close all; clc;
+
+mex -setup C++;
+mex mexfile.cpp;
+
+
+function GetPosition(X, Y)
+    mexfile 1;
+end
+
+function MoveAtVelocity(X, Y)
+    mexfile 2 X Y;
+end
+
+function MoveBack(X)
+    mexfile 3 X;
+end
+
+function MoveForward(X)
+    mexfile 4 X;
+end
+
+function MoveLeft(X)
+    mexfile 5 X;
+end
+
+function MoveRelative(X, Y)
+    mexfile 6 X Y;
+end
+
+function MoveRight(X)
+    mexfile 7 X;
+end
+
+function MoveToAbsolute(X, Y)
+    mexfile 8 X Y;
+end
+
+function MoveXAtVelocityUntilEncoderReference(X)
+    mexfile 9 X;
+end
+
+function MoveXToAbsolute(X)
+    mexfile 10 X;
+end
+
+function MoveYAtVelocityUntilEncoderReference(X)
+    mexfile 11 X;
+end
+
+function MoveYToAbsolute(X)
+    mexfile 12 X;
+end
+
+function RestoreReference()
+    mexfile 13;
+end
+
+function SetPosition(X, Y);
+    mexfile 14 X Y;
+end
+
+function SetReference()
+    mexfile 15;
+end
+
+function SkewAbout()
+    mexfile 16;
+end
+
+function SkewSecond()
+    mexfile 17;
+end
